baf103c8e7e71aa5aca8ff6650a2336496954e60
mohammadrezachegini <mohammadreza.chegini01@gmail.com>
2023-02-26 22:11:17 -0800

    change delete product to mongo

--- models/products.model.js
+++ models/products.model.js
@@ -3,12 +3,12 @@ const MongoConnection = require("../utils/mongo-connection")
 // const db = new MongoConnection()
 const fs = require('fs')
 const {ObjectId} = require("mongodb");
-
+const ProductCollection = "product"
 
 async function find() {
     const db = await new MongoConnection().Get();
     return new Promise(async (resolve, reject) => {
-        const products = await db.collection("product").find({
+        const products = await db.collection(ProductCollection).find({
             sort: {
                 _id: -1
             }
@@ -21,7 +21,7 @@ async function find() {
 async function findById(id) {
     const db = await new MongoConnection().Get();
     return new Promise(async (resolve, reject) => {
-        const product = await db.collection("product").findOne({_id: new ObjectId((id))})
+        const product = await db.collection(ProductCollection).findOne({_id: new ObjectId((id))})
         resolve(product)
     })
 
@@ -30,32 +30,19 @@ async function findById(id) {
 async function create(product) {
     const db = await new MongoConnection().Get();
     return new Promise(async (resolve, reject) => {
-        const result = await db.collection("products").insertOne(product)
+        const result = await db.collection(ProductCollection).insertOne(product)
         resolve(result);
     })
 
 }
 
 async function update(id,payload) {
-    return new Promise((resolve, reject) => {
-        products.map(product => {
-            if(product.id == id){
-                Object.assign(product,payload);
-
-            }
-
-            return product;
+    const db = await new MongoConnection().Get();
+    return new Promise(async (resolve, reject) => {
+        const result = await db.collection(ProductCollection).updateOne({_id: new ObjectId(id)},{
+            $set: {...product}
         })
-
-        fs.writeFile(`${process.cwd()}/data/products.json`, JSON.stringify(products), (err) => {
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve({message: "update product successfully "})
-            }
-
-        });
+        resolve(result);
 
     })
 
@@ -64,17 +51,10 @@ async function update(id,payload) {
 
 
 async function remove(id) {
-    return new Promise((resolve, reject) => {
-        const newList = products.filter(product => product.id != id);
-        fs.writeFile(`${process.cwd()}/data/products.json`, JSON.stringify(newList), (err) => {
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve({message: "deleted product successfully "})
-            }
-
-        });
+    const db = await new MongoConnection().Get();
+    return new Promise(async (resolve, reject) => {
+        const result = await db.collection(ProductCollection).deleteOne({_id: new ObjectId(id)})
+        resolve(result);
 
     })
 
